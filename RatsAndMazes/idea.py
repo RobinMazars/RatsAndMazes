cf14fec664f2f5be562fa0d20fc0a958f0706b48
RobinMazars <45461136+RobinMazars@users.noreply.github.com>
2019-06-08 16:33:29 +0200

    rewrite branch

---
+++ RatsAndMazes/idea.py
@@ -0,0 +1,104 @@
+import pprint
+
+pp = pprint.PrettyPrinter(indent=2).pprint
+
+
+def createBoard(nbrCaseH, nbrCaseV):
+    board = []
+    for x in range(nbrCaseV):
+        line = []
+        for y in range(nbrCaseH):
+            line.append([x+1, y+1])
+        board.append(line)
+    return board
+
+
+def createWall():
+    pass
+
+
+class Board():
+    def __init__(self, nbrCaseH, nbrCaseV):
+        self.board = []
+        self.pp = pprint.PrettyPrinter(indent=2).pprint
+        for x in range(nbrCaseV):
+            line = []
+            for y in range(nbrCaseH):
+                line.append([x+1, y+1])
+            self.board.append(line)
+
+    def show(self):
+        print('--------------------------------')
+        self.pp(self.board)
+        print('--------------------------------')
+
+    def getBoard(self):
+        return self.board
+
+    def clean(self, position):
+        pos = self.board[position.y][position.x]
+        self.board[position.y][position.x] = pos[:2]
+
+
+class Mj():
+    def __init__(self):
+        self.createBoard(5, 5)
+
+    def createBoard(self, nbrCaseH, nbrCaseV):
+        self.board = Board(nbrCaseH, nbrCaseV)
+
+    def createRat(self, position, name='R'):
+        self.rat = Rat(position, name)
+
+    def spawnRat(self):
+        RatPosition = self.rat.getPosition()
+        name = self.rat.name
+        self.board.getBoard()[RatPosition.y][RatPosition.x].append(name)
+
+    def moveRat(self, mouvX=0, mouvY=0):
+        self.cleanBoard(self.rat.getPosition())
+        self.rat.move(mouvX, mouvY)
+        self.spawnRat()
+
+    def cleanBoard(self, position):
+        self.board.clean(position)
+
+
+class Position():
+    def __init__(self, x, y):
+        self.x = x-1
+        self.y = y-1
+
+
+class Rat():
+    """docstring for Rat."""
+
+    def __init__(self, position, name='Rat'):
+        self.position = position
+        self.name = name
+
+    def getPosition(self):
+        return self.position
+
+    def move(self, mouvX=0, mouvY=0):
+        self.position.x += mouvX
+        self.position.y += mouvY
+        return self.position
+
+
+if __name__ == '__main__':
+    nbrCaseH = 8
+    nbrCaseV = nbrCaseH
+    nbrCaseTotal = nbrCaseH + nbrCaseV
+    origineX = 1
+    origineY = 2
+    mj = Mj()
+    mj.createBoard(nbrCaseH, nbrCaseV)
+    mj.createRat(Position(origineX, origineY))
+    mj.board.show()
+    mj.spawnRat()
+    mj.board.show()
+    mj.moveRat(1, 0)
+    mj.board.show()
+    mj.moveRat(0, 1)
+    mj.board.show()
---
+++ test/test_idea.py
@@ -0,0 +1,35 @@
+from RatsAndMazes.idea import Board
+from hypothesis import given, strategies as st
+
+#  pytest -v -l --hypothesis-show-statistics
+@given(nbrCaseH=st.integers(1, 100), nbrCaseV=st.integers(1, 100))
+def test_Board_Property(nbrCaseH, nbrCaseV):
+    board = Board(nbrCaseH, nbrCaseV)
+    assert(isinstance(board.getBoard(), type([])))
+    assert(len(board.getBoard()) == nbrCaseV)
+    for x in range(nbrCaseV):
+        assert(len(board.getBoard()[x]) == nbrCaseH)
+
+
+# @settings(max_examples=200,
+#           suppress_health_check=(HealthCheck.filter_too_much,))
+
+@st.composite
+def doubleIntegersLowerThan(draw):
+    int1 = draw(st.integers(1, 100))
+    int2 = draw(st.integers(1, int1))
+    return {'max': int1, 'min': int2}
+
+
+@given(HX=doubleIntegersLowerThan(),
+       VY=doubleIntegersLowerThan())
+def test_Board_clean(HX, VY):
+    # assume(positionX < nbrCaseV-1)
+    # assume(positionY < nbrCaseH-1)
+    dirtyBoard = Board(HX['max'], VY['max'])
+    cleanBoard = Board(HX['max'], VY['max'])
+    assert(cleanBoard.getBoard() == dirtyBoard.getBoard())
+    dirtyBoard.getBoard()[VY['min']-1][HX['min']-1].append("testRat")
+    print(HX['max'], HX['min'], VY['max'], VY['min'])
+    assert(cleanBoard.getBoard() != dirtyBoard.getBoard())
+    # assert(False)
